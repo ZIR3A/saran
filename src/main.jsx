16f2d8d3697922cfa45682644e8345a941d24b41
sharan baral <saranbrl35@gmail.com>
2026-08-22 09:18:14 +0545

    enhanced contents

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { applyThemeVariables } from './constants/applyTheme'
 import { resetScrollPosition } from './components/animations/scroll'
 import './index.css'
@@ -11,5 +12,6 @@ resetScrollPosition()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
